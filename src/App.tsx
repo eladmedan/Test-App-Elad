59e6a32b72a8bb4b3a7d6b50e347a57c0c12e448
eladmedan <eladmedan@gmail.com>
2024-09-05 12:02:39 +0300

    some more chabges

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,20 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { auth } from "./firebase";
-import Profile from "./components/Profile";
-import Home from "./components/Home";
-import AddPost from "./components/AddPost";
-import SignIn from "./components/SignIn";
-import { User as FirebaseUser } from "firebase/auth";
-import WelcomeScreen from "./components/WelcomeScreen";
-import BottomTabBar from "./components/BottomTabBar";
+import React, { useState } from 'react';
+import './App.css';
 
-const App: React.FC = () => {
-  const [user, setUser] = useState<FirebaseUser | null>(null);
-
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      setUser(user);
-    });
-    return () => unsubscribe();
-  }, []);
+function App() {
+  const [count, setCount] = useState(0);
 
   return (
-    <Router>
-      <div className="app flex flex-col min-h-screen">
-        <main className="flex-grow">
-          {user ? (
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/add-post" element={<AddPost />} />
-            </Routes>
-          ) : (
-            <Routes>
-              <Route path="/" element={<WelcomeScreen />} />
-              <Route path="/signin" element={<SignIn />} />
-            </Routes>
-          )}
-        </main>
-        {user && <BottomTabBar />}
+    <div className="App">
+      <h1>Welcome to my React app!</h1>
+      <p>This is a simple counter application.</p>
+      <div>
+        <button onClick={() => setCount(count - 1)}>-</button>
+        <span>{count}</span>
+        <button onClick={() => setCount(count + 1)}>+</button>
       </div>
-    </Router>
+    </div>
   );
-};
+}
 
 export default App;
