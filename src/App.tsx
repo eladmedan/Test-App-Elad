b270f85741c56c2d1b4341babd5ee9fd86953120
eladmedan <eladmedan@gmail.com>
2024-09-05 12:05:23 +0300

    some more changes 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    alert('Button clicked!');
+  };
 
   return (
     <div className="App">
-      <h1>Welcome to my React app!</h1>
-      <p>This is a simple counter application.</p>
-      <div>
-        <button onClick={() => setCount(count - 1)}>-</button>
-        <span>{count}</span>
-        <button onClick={() => setCount(count + 1)}>+</button>
-      </div>
+      <button className="big-button" onClick={handleClick}>
+        Click me
+      </button>
     </div>
   );
 }
